af16360855db988fbbe0d1ee2d6a0ad2a0861e33
Neruthes <i@neruthes.xyz>
2021-09-26 13:08:04 +0000

    Added postponemortalityalert.sh

---
+++ postponemortalityalert.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DATENOW=""
+NEWDATESTR="$(node -e 'console.log( (new Date(Date.now() + 20*24*3600*1000)).toISOString().slice(0,10) )')"
+echo "NEWDATESTR=$NEWDATESTR"
+
+sed "s/ALERTDATESTRING/$NEWDATESTR/" js/mortality-issue-tmpl.js > js/mortality-issue.js
