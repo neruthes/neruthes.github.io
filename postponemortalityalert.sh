7910945390c88c35b3cbd8e737a5d9abb62402f0
Neruthes <i@neruthes.xyz>
2021-09-27 12:40:21 +0000

    postponemortalityalert.sh: Auto commit and auto push

--- postponemortalityalert.sh
+++ postponemortalityalert.sh
@@ -5,3 +5,8 @@ NEWDATESTR="$(node -e 'console.log( (new Date(Date.now() + 20*24*3600*1000)).toI
 echo "NEWDATESTR=$NEWDATESTR"
 
 sed "s/ALERTDATESTRING/$NEWDATESTR/" js/mortality-issue-tmpl.js > js/mortality-issue.js
+
+
+git add js/mortality-issue.js
+git commit -m "Auto postpone death alert to $NEWDATESTR"
+git push
